063ab22e970459748ec3917f4faeb52afc39db15
fanyspenda <fanyervansyah.9c@gmail.com>
2021-02-06 16:40:50 +0700

    fix(onlineExperience): Online Experience Content

--- src/components/bigCardList.tsx
+++ src/components/bigCardList.tsx
@@ -3,22 +3,34 @@ import BigCardItem from "./bigCardItem";
 export interface BigCardListProps {
 	title: string;
 	description?: string;
+	isDarkMode?: boolean;
+	data: string[];
 }
 
 const BigCardList: React.FunctionComponent<BigCardListProps> = ({
 	title,
 	description,
+	isDarkMode,
+	data,
 }) => {
 	return (
-		<div className="pl-4 mt-8 flex flex-col">
-			<text className="text-xl font-bold mb-4">{title}</text>
-			<text>{description}</text>
+		<div
+			className={`pl-4 pt-8 pb-6 flex flex-col ${
+				isDarkMode && "bg-black text-white"
+			}`}
+		>
+			<text className="text-xl font-bold">{title}</text>
+			<text className="mb-4">{description}</text>
 			<div className="overflow-x-auto flex flex-row">
-				<BigCardItem title="Entire homes" />
-				<BigCardItem title="Cabbins and cottages" />
-				<BigCardItem title="Unique stay" />
-				<BigCardItem title="Pets welcome" />
+				{data.map((title) => (
+					<BigCardItem title={title} />
+				))}
 			</div>
+			{isDarkMode && (
+				<button className="p-1 border-gray-100 border-2 w-32 mt-6 rounded-lg">
+					Explore all
+				</button>
+			)}
 		</div>
 	);
 };
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,12 +3,22 @@ import Notification from "../components/notification";
 import bgJpg from "../bg.jpg";
 import bgWebp from "../bg.webp";
 import HeadingContent from "components/headingContent";
-import CardItem from "components/cardItem";
 import CardList from "components/cardList";
-import BigCardItem from "components/bigCardItem";
 import BigCardList from "components/bigCardList";
 
-export interface LandingPageProps {}
+const LIVE_EVERYWHERE_DATA = [
+	"Entire Homes",
+	"Cabbins and Cottages",
+	"Unique stay",
+	"Pets welcome",
+];
+
+const ONLINE_EXPERIENCES = [
+	"Make handmade pizza with Italian Grandmas",
+	"Practice mindful eating through zen philosophy",
+	"Get to know Argentina's famous Yerba Mate",
+	"Meditate to music with a parisian concert pianist",
+];
 
 const LandingPage: React.FunctionComponent = () => {
 	return (
@@ -20,13 +30,19 @@ const LandingPage: React.FunctionComponent = () => {
 			>
 				<source srcSet={bgWebp} type="image/webp" />
 				<source srcSet={bgJpg} type="image/jpeg" />
-				<img src={bgJpg} alt="Alt Text!" />
+				<img src={bgJpg} />
 			</picture>
 
 			<FormSearch />
 			<HeadingContent />
 			<CardList />
-			<BigCardList title="Live anywhere" />
+			<BigCardList title="Live anywhere" data={LIVE_EVERYWHERE_DATA} />
+			<BigCardList
+				data={ONLINE_EXPERIENCES}
+				isDarkMode
+				title="Online experiences"
+				description="interactive activities you can do together, led by expert hosts"
+			/>
 		</div>
 	);
 };
