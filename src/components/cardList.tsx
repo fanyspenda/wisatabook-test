af2218708b043ad1e1561c06fa7fd871942c84ed
fanyspenda <fanyervansyah.9c@gmail.com>
2021-02-07 12:42:38 +0700

    fix(content): fix horizontal destination card item UI for desktop view

--- src/components/cardList.tsx
+++ src/components/cardList.tsx
@@ -18,11 +18,27 @@ const itemData = [
 		name: "Sidoharjo",
 		duration: "3 hours",
 	},
+	{
+		name: "Jember",
+		duration: "1 hours",
+	},
+	{
+		name: "Sidoharjo",
+		duration: "3 hours",
+	},
+	{
+		name: "Jember",
+		duration: "1 hours",
+	},
+	{
+		name: "Sidoharjo",
+		duration: "3 hours",
+	},
 ];
 const CardList: React.FunctionComponent = () => {
 	const [destinations, setDestinations] = useState(itemData);
 	return (
-		<div className="flex px-4 flex-col overflow-x-auto max-h-56 flex-wrap mt-56">
+		<div className="flex px-4 flex-col items-center overflow-x-auto max-h-56 flex-wrap mt-56">
 			{destinations.map(({ name, duration }) => {
 				return <CardItem destination={name} duration={duration} />;
 			})}
